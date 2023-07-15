bcf6424226dfc9065b33328fb4a6c1183e05c549
serhatprogramming <serhatprogramming@gmail.com>
2023-07-15 06:42:05 -0400

    app.js separated

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+
+const playlistsRouter = require("./controllers/playlists");
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(config.dbUrl)
+  .then(() => logger.info("DB connection established"))
+  .catch(() => logger.error("Error connecting to database"));
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/playlists", playlistsRouter);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
+const app = require("./app");
 
 const logger = require("./utils/logger");
 const config = require("./utils/config");
-
-const playlistsRouter = require("./controllers/playlists");
-
-mongoose.set("strictQuery", false);
-mongoose
-  .connect(config.dbUrl)
-  .then(() => logger.info("DB connection established"))
-  .catch(() => logger.error("Error connecting to database"));
-
-app.use(cors());
-app.use(express.json());
-app.use("/api/playlists", playlistsRouter);
 
 app.listen(config.port, () => {
   logger.info(`Server listening on port ${config.port}`);
