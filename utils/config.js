374ff3340de2bc01747be8c5e36d6fd7d02d5952
serhatprogramming <serhatprogramming@gmail.com>
2023-08-07 17:10:01 -0400

    config updated with testing db

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 
 const config = {
-  dbUrl: process.env.DB_URL,
+  dbUrl:
+    process.env.NODE_ENV === "TEST_ENV"
+      ? process.env.TEST_DB_URL
+      : process.env.DB_URL,
   port: process.env.PORT || 3003,
 };
 
